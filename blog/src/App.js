8eab074f7d13bf6a030a5a2872e2ff3eee17b7de
kickbell <dev.kickbell@gmail.com>
2023-12-25 01:43:24 +0900

    Add 블로그 글 state 추가

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,16 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '강남 우동 맛집11'
+  let [글제목1, a] = useState('남자 코트 추천')
+  let [글제목2, b] = useState('여자 코트 추천')
+  let [글제목3, c] = useState('할버지 코트 추천')
+  let [logo, setLogo] = useState('ReactBlog')
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={ {color: 'red', fontSize : '26px'} }>블로그임</h4>
+        <h4>{ logo }</h4>
       </div> 
-      <h4>{post}</h4>
+      <div className="list">
+        <h4>{ 글제목1 }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목2 }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목3 }</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
