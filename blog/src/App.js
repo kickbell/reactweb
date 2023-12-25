54ca4057e4390809ddac9ecc72008092a0d5bd1d
kickbell <dev.kickbell@gmail.com>
2023-12-26 00:06:20 +0900

    Refactor State 하나로 동작하도록 수정

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,9 +5,7 @@ import { useState } from 'react';
 function App() {
 
   let post = '강남 우동 맛집11'
-  let [글제목1, a] = useState('남자 코트 추천')
-  let [글제목2, b] = useState('여자 코트 추천')
-  let [글제목3, c] = useState('할버지 코트 추천')
+  let [글제목, a] = useState(['남자 코트 추천', '여자 코트 추천', '할버지 코트 추천'])
   let [logo, setLogo] = useState('ReactBlog')
 
   return (
@@ -16,15 +14,15 @@ function App() {
         <h4>{ logo }</h4>
       </div> 
       <div className="list">
-        <h4>{ 글제목1 }</h4>
+        <h4>{ 글제목[0] }</h4>
+        <p>2월 17일 발행2</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{ 글제목2 }</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{ 글제목3 }</h4>
+        <h4>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
