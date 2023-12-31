4778c2ac230b87e68546017d93d402d243d9240c
kickbell <dev.kickbell@gmail.com>
2023-12-31 22:39:26 +0900

    Add data.js 에 더미데이터 추가

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,8 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Navbar, Container, Nav, Row, Col } from 'react-bootstrap'
+import { useState } from 'react';
+import data from './data.js';
 
 function App() {
+
+  let [shoes] = useState(data)
+  console.log(shoes[0].price)
+
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
@@ -23,8 +29,8 @@ function App() {
           <Row>
             <Col sm>
               <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-              <h4>상품명</h4>
-              <p>상품설명</p>
+              <h4>{shoes[0].title}</h4>
+              <p>{shoes[0].price}</p>
             </Col>
             <Col sm>
               <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
