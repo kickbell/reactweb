ee39d190b89b266a00f61eeed524fd3fd0f3c4d6
kickbell <dev.kickbell@gmail.com>
2023-12-31 22:01:47 +0900

    Add grid, image 넣기

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,9 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Navbar, Container, Nav, Row, Col } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">쇼핑몰</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+      <div>
+        <Container>
+          <Row>
+            <Col sm>
+              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+              <h4>상품명</h4>
+              <p>상품설명</p>
+            </Col>
+            <Col sm>
+              <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+              <h4>상품명</h4>
+              <p>상품설명</p>
+            </Col>
+            <Col sm>
+              <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+              <h4>상품명</h4>
+              <p>상품설명</p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+
     </div>
   );
 }
