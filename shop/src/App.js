4a952278cb565af81f711a7c828e39951ff5ca47
kickbell <dev.kickbell@gmail.com>
2024-01-02 01:38:59 +0900

    Fix 폴더구조 변경

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Navbar, Container, Nav, Row, Col } from 'react-bootstrap'
+import { Navbar, Container, Nav, Row } from 'react-bootstrap'
 import { useState } from 'react';
 import data from './data.js';
 import { Routes, Route, Link } from 'react-router-dom';
+import Detail from './pages/Detail.js'
+import Card from './components/Card.js'
+import NavigationBar from './components/Navbar.js'
 
 function App() {
 
@@ -12,16 +15,7 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">쇼핑몰</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      <NavigationBar/>
 
       <Link to="/">홈</Link>
       <Link to="/detail">상세페이지</Link>
@@ -35,7 +29,7 @@ function App() {
                 {
                   shoes.map((shoe, index) => {
                     return (
-                      <GridItem shoe={shoes[index]} idx={index} key={index} />
+                      <Card shoe={shoes[index]} idx={index} key={index} />
                     )
                   })
                 }
@@ -43,41 +37,11 @@ function App() {
             </Container>
           </>
         } />
-        <Route path="/detail" element={
-          <>
-            <div className="container">
-              <div className="row">
-                <div className="col-md-6">
-                  <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-                </div>
-                <div className="col-md-6">
-                  <h4 className="pt-5">상품명</h4>
-                  <p>상품설명</p>
-                  <p>120000원</p>
-                  <button className="btn btn-danger">주문하기</button>
-                </div>
-              </div>
-            </div>
-          </>
-        } />
+        <Route path="/detail" element={<Detail/>} />
       </Routes>
 
     </div>
   );
-}
-
-function Main() {
-
-}
-
-function GridItem(props) {
-  return (
-    <Col sm>
-      <img src={'https://codingapple1.github.io/shop/shoes' + (props.idx + 1) + '.jpg'} width="80%" />
-      <h4>{props.shoe.title}</h4>
-      <p>{props.shoe.price}</p>
-    </Col>
-  )
 }
 
 export default App;
---
+++ shop/src/components/Card.js
@@ -0,0 +1,13 @@
+import { Col } from 'react-bootstrap'
+
+function Card(props) {
+    return (
+        <Col sm>
+            <img src={'https://codingapple1.github.io/shop/shoes' + (props.idx + 1) + '.jpg'} width="80%" />
+            <h4>{props.shoe.title}</h4>
+            <p>{props.shoe.price}</p>
+        </Col>
+    )
+}
+
+export default Card
