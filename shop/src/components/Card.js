460c9d18b636244c38ecc58bdb327959a7881792
kickbell <dev.kickbell@gmail.com>
2024-01-03 01:05:22 +0900

    Fix Col->div로 바꾸어서 항상 3개로 로우가 유지되도록 변경

--- shop/src/components/Card.js
+++ shop/src/components/Card.js
@@ -2,11 +2,11 @@ import { Col } from 'react-bootstrap'
 
 function Card(props) {
     return (
-        <Col sm>
+        <div>
             <img src={'https://codingapple1.github.io/shop/shoes' + (props.idx + 1) + '.jpg'} width="80%" />
             <h4>{props.shoe.title}</h4>
             <p>{props.shoe.price}</p>
-        </Col>
+        </div>    
     )
 }
 
