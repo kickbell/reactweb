e1c33c4e9c16a28793c63c4451866a2822eba54b
kickbell <dev.kickbell@gmail.com>
2024-01-07 00:12:27 +0900

    Fix redux 누락된 import 추가

--- shop/src/index.js
+++ shop/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import store from './store.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,7 +15,6 @@ root.render(
         <App />
       </BrowserRouter>
     </Provider>
-
   </React.StrictMode>
 );
 
