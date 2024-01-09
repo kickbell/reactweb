366e237a47d8c0b84362ef3b75e27e6582e030d7
kickbell <dev.kickbell@gmail.com>
2024-01-10 02:50:13 +0900

    Add 재렌더링 막는 memo, useMemo 3

--- shop/src/components/Cart.js
+++ shop/src/components/Cart.js
@@ -3,28 +3,31 @@ import { useDispatch, useSelector } from 'react-redux';
 // import { changeName, increase } from '../store';
 // import { changeName, increase } from '../\bstore/userSlice';
 import { addCount } from '../\bstore/cartSlice';
-import { useState, memo } from 'react';
+import { useState, memo, useMemo } from 'react';
 
-let HeavyChild = memo( function(){
-    console.log('재렌더링됨')
-    return <div>자식임</div>
-})
+// let HeavyChild = memo( function(){
+//     console.log('재렌더링됨')
+//     return <div>자식임</div>
+// })
 
-// props가 변할때만 재랜더링
-
+// function 함수() {
+//     return '반복문10억번돌린결과'
+// }
 
 function Cart() {
     let cart = useSelector((state) => state.cart)
     let dispatch = useDispatch()
-    let [count, setCount] = useState(0)
+    // let [count, setCount] = useState(0)
+
+    // let result = useMemo(()=>{return 함수()}, [])
 
     // let state = useSelector((state) => state)
     // console.log(state)
 
     return (
         <Table>
-            <HeavyChild count={count}></HeavyChild>
-            <button onClick={()=>{ setCount(count+1)}}>+</button>
+            {/* <HeavyChild count={count}></HeavyChild> */}
+            {/* <button onClick={()=>{ setCount(count+1)}}>+</button> */}
             {/* <h6>{state.user.name}, {state.user.age}의 장바구니</h6>
             <button onClick={()=>{
                 dispatch(increase(10))
